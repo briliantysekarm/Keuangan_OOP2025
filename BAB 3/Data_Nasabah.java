f2dc2d72e231e497e8a494a9016e140ea5f3c7e5
briliantysekarm <briliantysekarmustikampus@gmail.com>
2025-04-13 21:38:57 +0700

    Add files via upload

---
+++ BAB 3/Data_Nasabah.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Keuangan.BAB3;
+
+import Keuangan.BAB2.*;
+
+/**
+ *
+ * @author brili
+ */
+public class Data_Nasabah {
+    String nama,norek,JenisKelamin, TglLahir, Alamat, Kontak;
+    
+    //menginisialisasi data nsabah dengan constructor
+    public Data_Nasabah(){
+        this.nama = "Brilianty Sekar Mustika";
+        this.TglLahir = "26-02-2006";
+        this.JenisKelamin = "Perempuan";
+        this.Alamat = "Jalan Dorowati Selatan No 96";
+        this.Kontak = "0898756787";
+        
+        
+    }
+}
---
+++ BAB 3/Transaksi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Keuangan.BAB3;
+
+/**
+ *
+ * @author brili
+ */
+public class Transaksi extends Data_Nasabah{
+    String kodeBank, jumlah, tanggalTransaksi;
+    
+
+    public Transaksi() {
+        super();
+        this.kodeBank = "";
+        this.jumlah = "";
+        this.tanggalTransaksi = "2025-03-26"; // default tanggal
+    
+    }
+}
