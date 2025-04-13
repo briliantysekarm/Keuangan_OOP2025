e07cc46d35be066ea029a416489281e3e8ba8346
briliantysekarm <briliantysekarmustikampus@gmail.com>
2025-04-13 19:15:04 +0700

    Add files via upload

---
+++ BAB 1/Cetak_Data.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Keuangan.BAB1;
+
+/**
+ *
+ * @author brili
+ */
+public class Cetak_Data {
+    public static void main(String[] args) {
+     // membuat object dengan nama “mhs” dari class Mahasiswa
+     Data_Nasabah nasabah = new Data_Nasabah(); 
+     //ketika akan menggunakan method dari class Mahasiswa maka harus menyertakan nama object
+     nasabah.dataNama("Brilianty Sekar Mustika");
+     nasabah.dataTglLahir("2001-01-03");
+     nasabah.dataJenisKelamin("Perempuan");
+     nasabah.dataAlamat("Jalan Dorowati Selatan No 26");
+     nasabah.dataKontak("Malang");
+     System.out.println("Data Nasabah Bank Sekar Pradana\n");
+     System.out.println("------------------------------------"); 
+     System.out.println("Nama           : "+ nasabah.cetakNama()); 
+     System.out.println("Tanggal Lahir  : "+ nasabah.cetakTglLahir()); 
+     System.out.println("Jenis Kelamin  : "+ nasabah.cetakJenisKelamin()); 
+     System.out.println("Alamat         : "+ nasabah.cetakAlamat()); 
+     System.out.println("Kontak         : "+ nasabah.cetakKontak()); 
+    }
+}
---
+++ BAB 1/Data_Nasabah.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Keuangan.BAB1;
+
+
+/**
+ *
+ * @author brili
+ */
+public class Data_Nasabah {
+    String nama,JenisKelamin, TglLahir, Alamat, Kontak;
+      
+    void dataNama(String Nama){
+        this.nama = Nama;
+    }
+    void dataJenisKelamin(String JenisKelamin){
+        this.JenisKelamin = JenisKelamin;
+    }  
+    void dataTglLahir(String TglLahir){
+        this.TglLahir = TglLahir;
+    }
+  
+    void dataAlamat(String alamat){
+        this.Alamat = alamat;
+    }  
+    
+    void dataKontak(String Kontak){
+        this.Kontak = Kontak;
+    } 
+    
+    String cetakNama(){
+        return nama;
+    }
+    String cetakJenisKelamin(){
+        return JenisKelamin;
+    }
+    String cetakTglLahir(){
+        return TglLahir;
+    }
+    
+    String cetakAlamat(){
+        return Alamat;
+    }
+    
+    String cetakKontak(){
+        return Kontak;
+    }
+
+    
+}
